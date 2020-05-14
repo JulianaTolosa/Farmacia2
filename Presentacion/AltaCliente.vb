9679cf5179ea7173da42d0b2eb69ab8a2df848fd
JulianaTolosa <julianatolosa4@gmail.com>
2020-05-14 19:03:39 -0300

    altacliente

--- Presentacion/AltaCliente.vb
+++ Presentacion/AltaCliente.vb
@@ -1,4 +1,9 @@
-﻿Public Class AltaCliente
+﻿Imports System.Data.SqlClient
+Imports System.Data.DataTable
+Imports System.Data
+Imports System.Text.RegularExpressions
+
+Public Class AltaCliente
     'Ejemplo 1
 
 
@@ -14,14 +19,75 @@
 
 
     Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If (String.IsNullOrEmpty(NombreTextBox.Text) Or String.IsNullOrEmpty(ApellidoTextBox.Text) Or String.IsNullOrEmpty(DireccionTextBox.Text) Or String.IsNullOrEmpty(DniTextBox.Text) Or String.IsNullOrEmpty(TelefonoTextBox.Text) Or String.IsNullOrEmpty(CelularTextBox.Text) Or String.IsNullOrEmpty(NacimientoTextBox.Text) Or String.IsNullOrEmpty(LocalidadTextBox.Text) Or String.IsNullOrEmpty(ProvinciaTextBox.Text) Or String.IsNullOrEmpty(ObraSocialTextBox.Text)) Then
+            MsgBox("Por Favor, rellene los campos vacios")
+        ElseIf Utilidades.ExisteCliente(DniTextBox.Text) Then
+            MessageBox.Show("El Cliente ya existe con este Documento, por favor intente nuevamente")
+            limpiar()
+        Else
+            Dim Nombre As String = NombreTextBox.Text
+            Dim Apellido As String = ApellidoTextBox.Text
+            Dim Direccion As String = DireccionTextBox.Text
+            Dim DNI As Integer = DniTextBox.Text
+            Dim Telefono As String = TelefonoTextBox.Text
+            Dim Celular As String = CelularTextBox.Text
+            Dim FechaNacimiento As Date = NacimientoTextBox.Text
+            Dim Localidad As String = LocalidadTextBox.Text
+            Dim Provincia As String = ProvinciaTextBox.Text
+            Dim IdObraSocial As String = ObraSocialTextBox.Text
 
+            Dim _Cliente As New Cliente()
+            _Cliente._Nombre = NombreTextBox.Text
+            _Cliente._Apellido = ApellidoTextBox.Text
+            _Cliente._Direccion = DireccionTextBox.Text
+            _Cliente._DNI = DniTextBox.Text
+            _Cliente._Telefono = TelefonoTextBox.Text
+            _Cliente._Celular = CelularTextBox.Text
+            _Cliente._FechaNacimiento = NacimientoTextBox.Text
+            _Cliente._Localidad = LocalidadTextBox.Text
+            _Cliente._Provincia = ProvinciaTextBox.Text
+            _Cliente._IdObraSocial = ObraSocialTextBox.Text
+
+            If Utilidades.AgregarCliente(_Cliente) Then
+                MsgBox("Cliente agregado correctamente!")
+            Else
+                MsgBox("Error al agregar Cliente!")
+            End If
+
+            limpiar()
+
+        End If
     End Sub
-
+    Public Sub limpiar()
+        NombreTextBox.Text = ""
+        ApellidoTextBox.Text = ""
+        DireccionTextBox.Text = ""
+        DniTextBox.Text = ""
+        NacimientoTextBox.Text = ""
+        LocalidadTextBox.Text = ""
+        ProvinciaTextBox.Text = ""
+        ObraSocialTextBox.Text = ""
+        DniTextBox.Focus()
+    End Sub
     Private Sub lblAltaCliente_Click(sender As Object, e As EventArgs) Handles lblAltaCliente.Click
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles DniTextBox.TextChanged
+    Private Sub DniTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles DniTextBox.KeyPress
+        If InStr(1, "0123456789" & Chr(8), e.KeyChar) = 0 Then
+            e.KeyChar = ""
+        End If
+    End Sub
 
+    'Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles DniTextBox.TextChanged
+
+    ' End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+   
+    Private Sub NacimientoTextBox_TextChanged(sender As Object, e As EventArgs) Handles NacimientoTextBox.TextChanged
+        
     End Sub
 End Class
